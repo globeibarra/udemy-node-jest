6a16a0c78f4995c6e321867dfd33465dda2e768e
Esteban Ibarra <esteban.ibarra@globant.com>
2022-05-10 12:04:18 -0500

    Adds test to call TodoModel.create in TodoController.createTodo

--- __tests__/unit/todo.controller.test.js
+++ __tests__/unit/todo.controller.test.js
@@ -1,7 +1,15 @@
+import {jest} from '@jest/globals';
 import TodoController from '../../controllers/todo.controller';
+import TodoModel from '../../model/todo.model';
+
+TodoModel.create = jest.fn();
 
 describe('TodoController.createTodo', () => {
     it('Should have a createTodo function', () => {
         expect(typeof TodoController.createTodo).toBe('function');
+    });
+    it('Should call TodoModel.create', () => {
+        TodoController.createTodo();
+        expect(TodoModel.create).toBeCalled();
     });
 });
