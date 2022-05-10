ee7f81791dc2d83af45775cc3fff8a91101ab159
Esteban Ibarra <esteban.ibarra@globant.com>
2022-05-10 12:44:29 -0500

    Refactors the body assignment to the request to be in scope of all the test of the controller

--- __tests__/unit/todo.controller.test.js
+++ __tests__/unit/todo.controller.test.js
@@ -14,16 +14,17 @@ beforeEach(() => {
 TodoModel.create = jest.fn();
 
 describe('TodoController.createTodo', () => {
+    beforeEach(() => {
+        req.body = newTodo;
+    });
     it('Should have a createTodo function', () => {
         expect(typeof TodoController.createTodo).toBe('function');
     });
     it('Should call TodoModel.create', () => {
-        req.body = newTodo;
         TodoController.createTodo(req, res, next);
         expect(TodoModel.create).toBeCalledWith(newTodo);
     });
     it('Should return 201 response code', () => {
-        req.body = newTodo;
         TodoController.createTodo(req, res, next);
         expect(res.statusCode).toBe(201);
         expect(res._isEndCalled()).toBeTruthy();
